bb805cb22b8812e712bc457bb6f23fc619e0b413
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2024-04-25 21:30:11 +0200

    init: copied from my old repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {UsersListComponent} from "./users/components/users-list/users-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UsersListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
