de699d8cc21a5f7409d6819d574c0340b0cdf3bb
ArtKanareyka <2pizzakanareyka@gmail.com>
2024-05-03 17:36:11 +0700

    .

--- src/app/pages/users/users-list.component.ts
+++ src/app/pages/users/users-list.component.ts
@@ -12,49 +12,49 @@ import { FormsModule } from '@angular/forms'
 import { MatFormFieldModule } from '@angular/material/form-field'
 
 @Component({
-	selector: 'app-user-list',
-	standalone: true,
-	imports: [
-		UserCardComponent,
-		MatGridListModule,
-		MatIconModule,
-		MatButtonModule,
-		MatFormFieldModule,
-		FormsModule,
-		CommonModule
-	],
-	templateUrl: './users-list.component.html'
+  selector: 'app-user-list',
+  standalone: true,
+  imports: [
+    UserCardComponent,
+    MatGridListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    FormsModule,
+    CommonModule
+  ],
+  templateUrl: './users-list.component.html'
 })
 export class UserListComponent implements OnInit {
-	private readonly dialog = inject(MatDialog)
-	public readonly usersService = inject(UsersService)
+  private readonly dialog = inject(MatDialog)
+  public readonly usersService = inject(UsersService)
 
-	openAddUserDialog(): void {
-		const dialogRef = this.dialog.open(CreateEditUserComponent, {
-			disableClose: true
-		})
+  openAddUserDialog(): void {
+    const dialogRef = this.dialog.open(CreateEditUserComponent, {
+      disableClose: true
+    })
 
-		dialogRef.afterClosed().subscribe(userFormData => {
-			if (userFormData) {
-				this.usersService.addUser(userFormData)
-			}
-		})
-	}
+    dialogRef.afterClosed().subscribe(userFormData => {
+      if (userFormData) {
+        this.usersService.addUser(userFormData)
+      }
+    })
+  }
 
-	openEditUserDialog(user: IUser): void {
-		const dialogRef = this.dialog.open(CreateEditUserComponent, {
-			disableClose: true,
-			data: user
-		})
+  openEditUserDialog(user: IUser): void {
+    const dialogRef = this.dialog.open(CreateEditUserComponent, {
+      disableClose: true,
+      data: user
+    })
 
-		dialogRef.afterClosed().subscribe(userFormData => {
-			if (userFormData) {
-				this.usersService.editUser(user, userFormData)
-			}
-		})
-	}
+    dialogRef.afterClosed().subscribe(userFormData => {
+      if (userFormData) {
+        this.usersService.editUser(user, userFormData)
+      }
+    })
+  }
 
-	ngOnInit(): void {
-		this.usersService.getUsers().subscribe()
-	}
+  ngOnInit(): void {
+    this.usersService.getUsers().subscribe()
+  }
 }
