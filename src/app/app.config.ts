3e6607d5b4226844aec715073e2dd9de8395981c
YusupOzdoyev <yusup.ozdoyev@mail.ru>
2024-05-13 13:51:44 +0300

    add effects from ngrx

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,14 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideStore } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
     provideAnimationsAsync(),
-    provideStore(reducers, { metaReducers })
+    provideStore(reducers, { metaReducers }),
+    provideEffects()
 ]
 };
