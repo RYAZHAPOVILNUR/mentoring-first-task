a49db771a5a37ebc0e870f6a86231965118a2aec
YusupOzdoyev <yusup.ozdoyev@mail.ru>
2024-05-14 11:22:40 +0300

    reser ngrx Revert "add effects from ngrx"
    This reverts commit 3e6607d5b4226844aec715073e2dd9de8395981c.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,14 +5,12 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideStore } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
-import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
     provideAnimationsAsync(),
-    provideStore(reducers, { metaReducers }),
-    provideEffects()
+    provideStore(reducers, { metaReducers })
 ]
 };
