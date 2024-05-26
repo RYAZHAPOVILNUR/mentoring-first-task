6dfb18e6ad40653f7bf8ca3041b1aea9f41989ae
baglana <102351104+baglana@users.noreply.github.com>
2024-05-26 14:13:18 +0500

    Создать компонент users-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {UsersListComponent} from "./components/users-list/users-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UsersListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
