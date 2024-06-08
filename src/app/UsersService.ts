8de972228f14f8bb2f63995ee793bdd36623399a
ravgusha <ravgusha@gmail.com>
2024-06-08 15:22:16 +0300

    sync state with localStorage (step 15)

--- src/app/UsersService.ts
+++ src/app/UsersService.ts
@@ -1,7 +1,8 @@
-import { BehaviorSubject, Observable } from "rxjs";
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+
 import { IUser } from "./user";
 import { UsersApiService } from './http.service';
-import { Injectable } from "@angular/core";
 
 @Injectable({
     providedIn: 'root'
@@ -13,30 +14,40 @@ export class usersService {
     constructor(private UsersApiService: UsersApiService) { }
 
     loadUsers() {
-        const users = localStorage.getItem('users');
-        if (users) {
-            this.usersSubject.next(JSON.parse(users))
-        } else {
-            this.UsersApiService.getUsers().subscribe(
-                (data: IUser[]) => {
-                    this.usersSubject.next(data)
-                    localStorage.setItem("users", JSON.stringify(data));
-                }
-            )
-        }
+        this.UsersApiService.getUsers().subscribe(
+            (data: IUser[]) => {
+                this.updateUsersState(data);
+            }
+        );
+    }
+
+    updateUsersState(data: IUser[]) {
+        this.usersSubject.next(data);
+        localStorage.setItem("users", JSON.stringify(data));
+    }
+
+    getUsers() {
+        const users: IUser[] = JSON.parse(localStorage.getItem("users")!);
+        if (users && users.length > 0) {
+            console.log(users.length);
+            this.usersSubject.next(users);
+        } else this.loadUsers();
     }
 
     createUser(user: IUser) {
-        this.usersSubject.next(this.usersSubject.getValue().concat([user]));
+        const updatedUsers = this.usersSubject.getValue().concat([user]);
+        this.updateUsersState(updatedUsers);
     }
 
     editUser(user: IUser) {
-        this.usersSubject.next(this.usersSubject.value.map(item =>
+        const updatedUsers = this.usersSubject.value.map(item =>
             item.id === user.id ? user : item
-        ));
+        );
+        this.updateUsersState(updatedUsers);
     }
 
     deleteUser(id: number) {
-        this.usersSubject.next(this.usersSubject.value.filter(user => user.id != id))
+        const updatedUsers = this.usersSubject.value.filter(user => user.id != id);
+        this.updateUsersState(updatedUsers);
     }
 }
--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,12 +1,13 @@
 import { AsyncPipe, NgFor } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+
 import { UserCard } from '../user-card/user-card.component';
 import { usersService } from '../../usersService';
-import { MatListModule } from '@angular/material/list';
 import { CreatEditUser } from "../create-edit-user/create-edit-user.component";
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { IUser } from '../../user';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
     selector: 'users-list',
@@ -40,24 +41,24 @@ export class UsersListComponent implements OnInit {
     openEditDialog(user: IUser) {
         const dialogRef = this.dialog.open(CreatEditUser, {
             data: user
-        })
+        });
 
         dialogRef.afterClosed().subscribe(res => {
-            this.onEditUser(res.data)
-        })
+            this.onEditUser(res.data);
+        });
     }
 
     openAddDialog() {
-        const dialogRef = this.dialog.open(CreatEditUser, {})
+        const dialogRef = this.dialog.open(CreatEditUser, {});
 
         dialogRef.afterClosed().subscribe(res => {
             const id = crypto.randomUUID();
-            const data = {...res.data, id: id}
-            this.onCreateUser(data)
-        })
+            const data = { ...res.data, id: id };
+            this.onCreateUser(data);
+        });
     }
 
     ngOnInit() {
-        this.usersService.loadUsers();
+        this.usersService.getUsers();
     }
 }
