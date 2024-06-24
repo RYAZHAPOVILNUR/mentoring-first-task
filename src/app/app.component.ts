adbb64001d2318cc0f95927cc519acd12ffa262b
ravgusha <ravgusha@gmail.com>
2024-06-24 15:27:46 +0300

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import { HttpClientModule } from "@angular/common/http";
-
-import { UsersApiService } from "./http.service";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule],
-  providers: [UsersApiService],
+  imports: [RouterOutlet],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
--- src/app/store/effects.ts
+++ src/app/store/effects.ts
@@ -10,12 +10,13 @@ import { IUser } from "../user";
 @Injectable()
 export class UsersEffects {
   constructor(private actions$: Actions, private http: HttpClient) { }
+  private BASE_URL = 'https://jsonplaceholder.typicode.com/users';
 
   loadData$ = createEffect(() =>
     this.actions$.pipe(
       ofType(UsersActions.getUsers),
       switchMap(() =>
-        this.http.get<IUser[]>('https://jsonplaceholder.typicode.com/users').pipe(
+        this.http.get<IUser[]>(this.BASE_URL + '/users').pipe(
           map(users => UsersActions.getUsersSuccess({ users: users })),
           catchError(error => of(UsersActions.getUsersFailure({ error: error.message })))
         )
